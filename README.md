eb5bf4e7b4b07d0924e00ccb105ca924fb09b25d
sushma95 <54374529+sushma95@users.noreply.github.com>
2020-05-19 10:47:37 -0500

    add maven archetype

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # kafka-java-app
+
+## Team Members
+
+- Sushma Yedugani(Team Lead)
+
+<br>
+
+
+- Deepak Malempati
+
+<br>
+
+- Deepthi Tejaswani Chokka
+
+<br>
+
+- Dheeraj Edupuganti
+
+<br>
+
+- Maha Lakshmi Kongari
+
+
---
+++ src/main/java/edu/nwmissouri/bigdata2/App.java
@@ -0,0 +1,13 @@
+package edu.nwmissouri.bigdata2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello This is Group 2 here, Hope everyone is doing great" );
+    }
+}
