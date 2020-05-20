fa3b9f05a39eefaf8394706c3f746af402678734
Mahalakshmi <mahalakshmi.kongari@gmail.com>
2020-05-20 15:16:31 -0500

    Edited Producer.java file

--- src/main/java/edu/nwmissouri/bigdata2/Producer.java
+++ src/main/java/edu/nwmissouri/bigdata2/Producer.java
@@ -8,55 +8,34 @@ import java.util.Properties;
 import java.util.Scanner;
 
 /**
- * Created by sunilpatil on 12/28/15. Modified by Denise Case on 10/29/2019.
+ * Created by sunilpatil on 12/28/15.
  */
-public class ProducerHello {
-  private static Scanner in;
+public class Producer {
+    private static Scanner in;
+    public static void main(String[] argv)throws Exception {
+        if (argv.length != 1) {
+            System.err.println("Please specify 1 parameters ");
+            System.exit(-1);
+        }
+        String topicName = argv[0];
+        in = new Scanner(System.in);
+        System.out.println("Enter message(type exit to quit)");
 
-  public static void main(String[] argv) throws Exception {
-    if (argv.length != 1) {
-      System.err.println("Please specify 1 parameter (the name of the topic)");
-      System.exit(-1);
+        //Configure the Producer
+        Properties configProperties = new Properties();
+        configProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
+        configProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serialization.ByteArraySerializer");
+        configProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serialization.StringSerializer");
+
+        org.apache.kafka.clients.producer.Producer producer = new KafkaProducer(configProperties);
+        String line = in.nextLine();
+        while(!line.equals("exit")) {
+            //TODO: Make sure to use the ProducerRecord constructor that does not take parition Id
+            ProducerRecord<String, String> rec = new ProducerRecord<String, String>(topicName,line);
+            producer.send(rec);
+            line = in.nextLine();
+        }
+        in.close();
+        producer.close();
     }
-    String topicName = argv[0];
-    in = new Scanner(System.in);
-    System.out.println("Thank you for providing the topic " + topicName + "\n");
-    System.out.println("Enter message (type exit to quit).\n");
-
-    // Configure the Producer
-    Properties configProperties = new Properties();
-    configProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-    configProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
-        "org.apache.kafka.common.serialization.ByteArraySerializer");
-    configProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
-        "org.apache.kafka.common.serialization.StringSerializer");
-    System.out.println("The configuration properties are: " + configProperties.toString());
-    System.out.println("\nWill use this configuration to create a producer.\n");
-
-    org.apache.kafka.clients.producer.Producer producer = new KafkaProducer(configProperties);
- 
-
-    
-    for (int i = 1; i <= 5; ++i) 
-    {
-      for(int j = 1; j<=i; ++j)
-      {
-      String message = "* ";
-      ProducerRecord<String, String> rec = new ProducerRecord<String, String>(topicName, message);
-      producer.send(rec);
-    }}
-
-
-    // still allow input from keyboard
-
-    String line = in.nextLine();
-    while (!line.equals("exit")) {
-      ProducerRecord<String, String> rec = new ProducerRecord<String, String>(topicName, line);
-      producer.send(rec);
-      line = in.nextLine();
-    }
-
-    in.close();
-    producer.close();
-  }
 }
